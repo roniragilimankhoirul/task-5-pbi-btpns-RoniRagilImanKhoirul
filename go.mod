1a57fd43d333cdd09fec0a7ce53862612c04a68f
roniragilimankhoirul <ronisensei7@gmail.com>
2023-09-03 15:27:52 +0700

    menambahkan helpers

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module task-5-pbi-btpns-RoniRagilImanKhoirul
 go 1.21.0
 
 require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
 	github.com/bytedance/sonic v1.10.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.0 // indirect
@@ -13,6 +14,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.15.3 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
---
+++ helpers/helpers.go
@@ -0,0 +1,59 @@
+package helpers
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// SECRET adalah kunci rahasia yang digunakan untuk penandatanganan token JWT.
+var SECRET = "LKAJLKDFJLJalkjdlfajslkdfj123130124"
+
+// Validation adalah fungsi untuk memvalidasi data menggunakan govalidator.
+// Ini mengembalikan error jika validasi gagal.
+func Validation(c *gin.Context, data interface{}) error {
+	_, err := govalidator.ValidateStruct(data)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": err.Error(),
+		})
+		return err
+	}
+	return err
+}
+
+// Encrypt_password adalah fungsi untuk mengenkripsi kata sandi menggunakan bcrypt.
+// Ini mengembalikan string yang dienkripsi.
+func Encrypt_password(c *gin.Context, password string) string {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "gagal mengenkripsi kata sandi"})
+		return ""
+	}
+	return string(hash)
+}
+
+// Check_password adalah fungsi untuk memeriksa kecocokan kata sandi terenkripsi dengan kata sandi asli.
+// Ini mengembalikan error jika kata sandi tidak cocok.
+func Check_password(pass_1 string, pass_2 string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(pass_1), []byte(pass_2))
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+// Initialize_token adalah fungsi untuk menginisialisasi token JWT dengan ID pengguna.
+// Ini mengembalikan token yang ditandatangani dan error jika terjadi kesalahan.
+func Initialize_token(userid string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": userid,
+		"exp": time.Now().Add(time.Minute * 1).Unix(),
+	})
+	tokenStr, err := token.SignedString([]byte(SECRET))
+	return tokenStr, err
+}
