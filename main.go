540c8d0e81e6991199d4c00c5769c96a18e4dfcd
roniragilimankhoirul <ronisensei7@gmail.com>
2023-09-03 15:48:12 +0700

    menambahkan main file pada project

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	database "task-5-pbi-btpns-RoniRagilImanKhoirul/database"
+)
+
+func main() {
+	var r = gin.Default()
+	database.ConnectDb()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, Gin!",
+		})
+	})
+
+	r.Run()
+}
